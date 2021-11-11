12a30610ff78a24bbce51b37571a95d9b896a4d8
rifnur <rifnur69v@mail.ru>
2021-11-11 19:55:27 +0300

    Урок 7 Упражнение Реализовать программу, в которой задается граф из 10 вершин. Задать ребра и найти кратчайший путь с помощью поиска в ширину.

--- src/main/java/Lesson7/Main.java
+++ src/main/java/Lesson7/Main.java
@@ -2,12 +2,25 @@ package Lesson7;
 
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph(5);
-        graph.addEdge(0,2);
-        graph.addEdge(1,3);
-        graph.addEdge(3,2);
-        graph.addEdge(4,2);
-        graph.addEdge(4,3);
+        Graph graph = new Graph(10);
+        graph.addEdge(2,3);
+        graph.addEdge(3,9);
+        graph.addEdge(3,4);
+        graph.addEdge(4,6);
+        graph.addEdge(6,7);
+        graph.addEdge(6,8);
+        graph.addEdge(6,0);
+        graph.addEdge(5,9);
+        graph.addEdge(5,1);
+        BreadthFirstPaths bfp = new BreadthFirstPaths(graph,1);
+        System.out.println(bfp.hasPathTo(2));
+        System.out.println(bfp.pathTo(2));
+
+//        graph.addEdge(0,2);
+//        graph.addEdge(1,3);
+//        graph.addEdge(3,2);
+//        graph.addEdge(4,2);
+//        graph.addEdge(4,3);
 //        System.out.println(graph.getEdgeCount());
 //        System.out.println(graph.getAdjList(2));
 
@@ -15,9 +28,9 @@ public class Main {
 //        System.out.println(dfp.hasPathTo(0));
 //        System.out.println(dfp.pathTo(0));
 
-        BreadthFirstPaths bfp = new BreadthFirstPaths(graph,1);
-        System.out.println(bfp.hasPathTo(0));
-        System.out.println(bfp.pathTo(0));
+//        BreadthFirstPaths bfp = new BreadthFirstPaths(graph,1);
+//        System.out.println(bfp.hasPathTo(0));
+//        System.out.println(bfp.pathTo(0));
 
     }
 }
